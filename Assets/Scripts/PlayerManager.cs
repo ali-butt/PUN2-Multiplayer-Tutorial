2575f52813b44dabe08343d73ee9751a4720e610
Ali Butt <aliiftikharbutt@gmail.com>
2025-01-19 21:17:13 +0500

    sdc

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviourPunCallbacks
 {
     [Tooltip("Beam of light")]
     [SerializeField] GameObject Beam;
 
     bool IsFiring;
+
+    public float Health = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +21,62 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1") && !IsFiring)
+        if (Input.GetButtonDown("Fire1") && !IsFiring)
         {
             IsFiring = true;
         }
-        
-        if(Input.GetButtonUp("Fire1") && IsFiring)
+
+        if (Input.GetButtonUp("Fire1") && IsFiring)
         {
             IsFiring = false;
         }
 
-        if(Beam && IsFiring != Beam.activeInHierarchy)
+        if (Beam && IsFiring != Beam.activeInHierarchy)
         {
             Beam.SetActive(IsFiring);
         }
+    }
+
+
+    /// <summary>
+    /// MonoBehaviour method called when the Collider 'other' enters the trigger.
+    /// Affect Health of the Player if the collider is a beam
+    /// Note: when jumping and firing at the same, you'll find that the player's own beam intersects with itself
+    /// One could move the collider further away to prevent this or check if the beam belongs to the player.
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        // We are only interested in Beamers
+        // we should be using tags but for the sake of distribution, let's simply check by name.
+        if (!other.name.Contains("Beam"))
+        {
+            return;
+        }
+        Health -= 0.1f;
+    }
+    /// <summary>
+    /// MonoBehaviour method called once per frame for every Collider 'other' that is touching the trigger.
+    /// We're going to affect health while the beams are touching the player
+    /// </summary>
+    /// <param name="other">Other.</param>
+    void OnTriggerStay(Collider other)
+    {
+        // we dont' do anything if we are not the local player.
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+        // We are only interested in Beamers
+        // we should be using tags but for the sake of distribution, let's simply check by name.
+        if (!other.name.Contains("Beam"))
+        {
+            return;
+        }
+        // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
+        Health -= 0.1f * Time.deltaTime;
     }
 }
